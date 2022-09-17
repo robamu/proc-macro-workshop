1210326e6c64f354f011a2569ada33ccdb83c9a0
Robin Mueller <robin.mueller.m@gmail.com>
2022-09-17 20:42:26 +0200

    init commit

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,22 @@
+use syn::{parse_macro_input, DeriveInput};
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::{quote, ToTokens};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident.to_token_stream();
+    let builder_ident = Ident::new(&format!("{}Builder", input.ident), Span::call_site());
+    let tokens = quote! {
+        impl #ident {
+            pub fn builder() {}
+        }
 
-    unimplemented!()
+        pub struct #builder_ident {
+
+        }
+    };
+    TokenStream::from(tokens)
 }
