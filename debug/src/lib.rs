591a072c49571e64c840e35b1eee04bbb01ebd33
Robin Mueller <muellerr@irs.uni-stuttgart.de>
2022-09-22 21:27:55 +0200

    some refactoring

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,5 +1,37 @@
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Meta};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Lit, Meta};
+
+fn handle_field(field: &Field, field_formatters: &mut Vec<TokenStream>) -> syn::Result<()> {
+    if let Some(fident) = &field.ident {
+        let mut field_modifier = None;
+        for attr in &field.attrs {
+            match attr.parse_meta()? {
+                Meta::NameValue(meta_nv) => {
+                    if let Lit::Str(lit_str) = &meta_nv.lit {
+                        field_modifier = Some(lit_str.value());
+                    } else {
+                        unimplemented!();
+                    }
+                }
+                _ => {
+                    unimplemented!();
+                }
+            }
+        }
+        let fident_str = fident.to_string();
+        if let Some(modifier) = field_modifier {
+            field_formatters.push(quote! {
+                .field(#fident_str, &format_args!(#modifier, &self.#fident))
+            })
+        } else {
+            field_formatters.push(quote! {
+                .field(#fident_str, &self.#fident)
+            })
+        }
+    }
+    Ok(())
+}
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -7,33 +39,20 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let struct_ident = &input.ident;
     let ident_as_str = struct_ident.to_string();
 
-    let mut field_idents = Vec::new();
+    let mut field_formatters = Vec::new();
 
-    for attr in input.attrs {
-        match attr.parse_meta()? {
-            Meta::Path(_) => {}
-            Meta::List(_) => {}
-            Meta::NameValue(_) => {}
-        }
-    }
     match input.data {
         Data::Struct(s_data) => {
             for field in s_data.fields {
-                if let Some(fident) = field.ident {
-                    field_idents.push(fident)
+                match handle_field(&field, &mut field_formatters) {
+                    Ok(_) => {}
+                    Err(e) => return e.into_compile_error().into(),
                 }
             }
         }
         _ => {
             unimplemented!();
         }
-    }
-    let mut field_formatters = Vec::new();
-    for fident in field_idents {
-        let fident_str = fident.to_string();
-        field_formatters.push(quote! {
-            .field(#fident_str, &self.#fident)
-        })
     }
     let output = quote! {
         use core::fmt;
