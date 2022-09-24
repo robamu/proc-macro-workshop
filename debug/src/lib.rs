7981c65f9e12a90c920b0d9f4474989c94436a39
Robin Mueller <robin.mueller.m@gmail.com>
2022-09-24 16:51:10 +0200

    thats a weird error..

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,9 +1,22 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Ident, TokenStream};
 use quote::quote;
+use std::collections::HashMap;
 use syn::spanned::Spanned;
-use syn::{parse_macro_input, Data, DeriveInput, Field, GenericParam, Lit, Meta};
+use syn::{
+    parse_macro_input, Data, DeriveInput, Field, GenericArgument, GenericParam, Lit, Meta,
+    PathArguments, Type,
+};
 
-fn handle_field(field: &Field, field_formatters: &mut Vec<TokenStream>) -> syn::Result<()> {
+#[derive(Default)]
+struct GenericsInfo {
+    no_trait_bound_generation: bool,
+}
+
+fn handle_field(
+    field: &Field,
+    field_formatters: &mut Vec<TokenStream>,
+    generic_idents: &mut HashMap<Ident, GenericsInfo>,
+) -> syn::Result<()> {
     if let Some(fident) = &field.ident {
         let mut field_modifier = None;
         for attr in &field.attrs {
@@ -12,13 +25,41 @@ fn handle_field(field: &Field, field_formatters: &mut Vec<TokenStream>) -> syn::
                     if let Lit::Str(lit_str) = &meta_nv.lit {
                         field_modifier = Some(lit_str.value());
                     } else {
-                        unimplemented!();
+                        return Err(syn::Error::new(
+                            meta_nv.span(),
+                            "Expected literal string argument",
+                        ));
                     }
                 }
                 _ => {
-                    unimplemented!();
+                    return Err(syn::Error::new(attr.span(), "Expected name value pair"));
                 }
             }
+        }
+        match &field.ty {
+            Type::Path(ty_path) => {
+                if let Some(ty_seg) = ty_path.path.segments.first() {
+                    // Common special case: Do not emit trait bound T: Debug if T is only used
+                    // inside PhantomData
+                    if ty_seg.ident == "PhantomData" {
+                        if let PathArguments::AngleBracketed(gen_args) = &ty_seg.arguments {
+                            for arg in &gen_args.args {
+                                if let GenericArgument::Type(Type::Path(generic_path)) = arg {
+                                    if let Some(generic_type) = generic_path.path.segments.first() {
+                                        if generic_idents.contains_key(&generic_type.ident) {
+                                            generic_idents
+                                                .get_mut(&generic_type.ident)
+                                                .unwrap()
+                                                .no_trait_bound_generation = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            _ => {}
         }
         let fident_str = fident.to_string();
         if let Some(modifier) = field_modifier {
@@ -41,10 +82,9 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let ident_as_str = struct_ident.to_string();
 
     let mut field_formatters = Vec::new();
+    let mut generic_idents = HashMap::new();
 
-    let mut impl_tt = None;
     for generic in &input.generics.params {
-        let (impl_generics, type_generics, _) = input.generics.split_for_impl();
         let gen_ident = if let GenericParam::Type(ty) = generic {
             &ty.ident
         } else {
@@ -52,28 +92,44 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
                 .to_compile_error()
                 .into();
         };
-        impl_tt = Some(quote! {
-            impl #impl_generics fmt::Debug for #struct_ident #type_generics
-                where #gen_ident: fmt::Debug
-        });
+        generic_idents.insert(gen_ident.clone(), GenericsInfo::default());
     }
-    if impl_tt.is_none() {
-        impl_tt = Some(quote! { impl fmt::Debug for #struct_ident });
-    }
-    let impl_tt = impl_tt.unwrap();
+
     match input.data {
         Data::Struct(s_data) => {
             for field in s_data.fields {
-                match handle_field(&field, &mut field_formatters) {
+                match handle_field(&field, &mut field_formatters, &mut generic_idents) {
                     Ok(_) => {}
                     Err(e) => return e.into_compile_error().into(),
                 }
             }
         }
         _ => {
-            unimplemented!();
+            return syn::Error::new(input.span(), "Can only use on regular data structs")
+                .into_compile_error()
+                .into()
         }
     }
+    let mut trait_bounds = Vec::new();
+    if !generic_idents.is_empty() {
+        for (ident, info) in generic_idents {
+            if !info.no_trait_bound_generation {
+                trait_bounds.push(quote! {
+                   #ident: fmt::Debug
+                });
+            }
+        }
+    }
+    let impl_tt = if trait_bounds.is_empty() {
+        quote! { impl fmt::Debug for #struct_ident }
+    } else {
+        let (impl_generics, type_generics, _) = input.generics.split_for_impl();
+        quote! {
+            impl #impl_generics fmt::Debug for #struct_ident #type_generics where
+                #(#trait_bounds),*
+        }
+    };
+
     let output = quote! {
         use core::fmt;
 
