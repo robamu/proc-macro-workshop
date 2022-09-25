d584f14898a7e2e414a1209b9baf1b5c6952de33
Robin Mueller <robin.mueller.m@gmail.com>
2022-09-25 23:56:29 +0200

    very basic parsing

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,45 +1,36 @@
+use proc_macro2::TokenStream;
 use syn::parse::{Parse, ParseStream};
-use syn::punctuated::Punctuated;
-use syn::{braced, parse_macro_input, token, DeriveInput, Field, Ident, Token};
+use syn::{braced, parse_macro_input, Ident, Token};
 
-enum Item {
-    SeqStruct(SeqStruct),
-}
-
-struct SeqStruct {
+struct SeqInfo {
     loop_ident: Ident,
-    in_token: Token![in],
     lit_start: syn::LitInt,
-    range_token: Token![..],
     lit_end: syn::LitInt,
+    content: TokenStream,
 }
 
-impl Parse for Item {
+impl Parse for SeqInfo {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let lookahead = input.lookahead1();
-        if lookahead.peek(Ident) {
-            input.parse().map(Item::SeqStruct)
-        } else {
-            Err(lookahead.error())
-        }
-    }
-}
-
-impl Parse for SeqStruct {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        Ok(SeqStruct {
-            loop_ident: input.parse()?,
-            in_token: input.parse()?,
-            lit_start: input.parse()?,
-            range_token: input.parse()?,
-            lit_end: input.parse()?,
+        let loop_ident = input.parse()?;
+        input.parse::<Token![in]>()?;
+        let lit_start = input.parse()?;
+        input.parse::<Token![..]>()?;
+        let lit_end = input.parse()?;
+        let content;
+        braced!(content in input);
+        let content = content.parse()?;
+        Ok(SeqInfo {
+            loop_ident,
+            lit_start,
+            lit_end,
+            content,
         })
     }
 }
 
 #[proc_macro]
 pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    //let input = parse_macro_input!(input as Item);
+    let input = parse_macro_input!(input as SeqInfo);
 
     proc_macro::TokenStream::new()
 }
