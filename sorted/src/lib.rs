43b7f38b968651be13f956bce547e1f471336ae0
Robin Mueller <robin.mueller.m@gmail.com>
2022-10-01 16:50:52 +0200

    clean up a bit

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,8 +1,20 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn sorted(
+    args: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let _ = args;
-    let _ = input;
-    TokenStream::new().into()
+    let input_copy = input.clone();
+    let input_item = parse_macro_input!(input_copy as syn::Item);
+    process_input(input_item)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
+
+fn process_input(input: syn::Item) -> syn::Result<TokenStream> {
+    Ok(input.to_token_stream())
 }
