fcf55cbb37efcdd453bb6fef0e03756185352e74
Robin Mueller <robin.mueller.m@gmail.com>
2022-10-01 15:33:20 +0200

    05 and 06 passing

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -64,19 +64,8 @@ fn gen_output(input: SeqInfo) -> syn::Result<TokenStream> {
     // If this pattern is found, we need to repeat the inner repitition instead of repeating the
     // whole content. The TokenBuffer is suited for this task
     let tok_buf = TokenBuffer::new2(input.content);
-    let mut current_cursor = Cursor::empty();
-    let mut inner_repitition_found = false;
-
-    while !current_cursor.eof() {
-        if let Some((punct, _)) = current_cursor.punct() {
-            if punct.as_char() == '#' && current_cursor.group(Bracket).is_some() {
-                inner_repitition_found = true;
-                break;
-            }
-        }
-        let (_, next) = current_cursor.token_tree().unwrap();
-        current_cursor = next;
-    }
+    let mut current_cursor = tok_buf.begin();
+    let inner_repitition_found = recursive_inner_reps_check(&mut current_cursor);
     Ok(if !inner_repitition_found {
         for next_num in start..end {
             current_cursor = tok_buf.begin();
@@ -91,11 +80,61 @@ fn gen_output(input: SeqInfo) -> syn::Result<TokenStream> {
         }
     } else {
         let mut tt_collector = TtCollectorInnerReps::new(&input.loop_ident, start, end);
+        current_cursor = tok_buf.begin();
         while !current_cursor.eof() {
-            // TODO: Implement
+            tt_collector.handle_cursor(&mut current_cursor);
         }
-        quote! {}
+        tt_collector.consume()
     })
+}
+
+fn recursive_inner_reps_check(current_cursor: &mut Cursor) -> bool {
+    while !current_cursor.eof() {
+        if check_for_inner_reps(current_cursor) {
+            return true;
+        }
+
+        if group_checks(current_cursor) {
+            return true;
+        }
+        let (_, next_cursor) = current_cursor.token_tree().unwrap();
+        *current_cursor = next_cursor;
+    }
+    false
+}
+
+fn group_checks(current_cursor: &Cursor) -> bool {
+    if let Some((mut gcursor, _, _)) = current_cursor.group(Brace) {
+        if recursive_inner_reps_check(&mut gcursor) {
+            return true;
+        }
+    }
+    if let Some((mut gcursor, _, _)) = current_cursor.group(Parenthesis) {
+        if recursive_inner_reps_check(&mut gcursor) {
+            return true;
+        }
+    }
+    if let Some((mut gcursor, _, _)) = current_cursor.group(Bracket) {
+        if recursive_inner_reps_check(&mut gcursor) {
+            return true;
+        }
+    }
+    false
+}
+
+fn check_for_inner_reps(current_cursor: &Cursor) -> bool {
+    if let Some((punct, next_cursor)) = current_cursor.punct() {
+        if punct.as_char() == '#' {
+            if let Some((_, _, next_cursor)) = next_cursor.group(Parenthesis) {
+                if let Some((punct, _)) = next_cursor.punct() {
+                    if punct.as_char() == '*' {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    false
 }
 
 impl<'a> TtCollectorBase<'a> {
@@ -162,7 +201,6 @@ impl<'a> TtCollectorDefault<'a> {
         let (tt, next_cursor) = current_cursor
             .token_tree()
             .expect("Cursor parsing configuration error. Reached unexpected EOF");
-        // dbg!("Pushing TT {}", &tt);
         self.base.collected_tokens.push(tt);
         *current_cursor = next_cursor;
     }
@@ -184,11 +222,152 @@ impl<'a> TtCollectorInnerReps<'a> {
         Self {
             base: TtCollectorBase::new(loop_ident),
             start,
-            end
+            end,
         }
     }
 
     fn consume(self) -> TokenStream {
         self.base.consume()
+    }
+
+    fn consume_as_tok_vec(self) -> Vec<TokenTree> {
+        self.base.collected_tokens
+    }
+
+    fn handle_cursor(&mut self, current_cursor: &mut Cursor) {
+        if let Some((punct, cursor_after_hash)) = current_cursor.punct() {
+            if punct.as_char() == '#' {
+                if let Some((gcursor, _, cursor_after_group)) = cursor_after_hash.group(Parenthesis)
+                {
+                    if let Some((punct, next_cursor)) = cursor_after_group.punct() {
+                        if punct.as_char() == '*' {
+                            for next_idx in self.start..self.end {
+                                let mut group_cursor_local = gcursor;
+                                let mut inner_collector = TtCollectorInnerReps::new(
+                                    self.base.loop_ident,
+                                    self.start,
+                                    self.end,
+                                );
+                                while !group_cursor_local.eof() {
+                                    inner_collector
+                                        .handle_inner_rep_cursor(&mut group_cursor_local, next_idx);
+                                }
+                                self.base
+                                    .collected_tokens
+                                    .append(&mut inner_collector.consume_as_tok_vec());
+                            }
+                            *current_cursor = next_cursor;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        if self.check_outer_groups(current_cursor) {
+            return;
+        }
+        let (tt, next_cursor) = current_cursor
+            .token_tree()
+            .expect("Cursor parsing configuration error. Reached unexpected EOF");
+        self.base.collected_tokens.push(tt);
+        *current_cursor = next_cursor;
+    }
+
+    fn check_outer_groups(&mut self, current_cursor: &mut Cursor) -> bool {
+        let mut group_check = |delim| {
+            if let Some((mut group_cursor, gspan, next)) = current_cursor.group(delim) {
+                self.handle_group_cursor_outer(delim, &mut group_cursor, gspan);
+                *current_cursor = next;
+                return true;
+            }
+            false
+        };
+        if group_check(Parenthesis) {
+            return true;
+        }
+        if group_check(Brace) {
+            return true;
+        }
+        if group_check(Bracket) {
+            return true;
+        }
+        false
+    }
+
+    fn handle_inner_rep_cursor(&mut self, group_cursor: &mut Cursor, current_index: usize) {
+        if let Some((ident, next_cursor)) = group_cursor.ident() {
+            if &ident == self.base.loop_ident {
+                let lit_int = LitInt::new(&current_index.to_string(), Span::call_site());
+                self.base.collected_tokens.push(lit_int.token().into());
+                *group_cursor = next_cursor;
+                return;
+            }
+        }
+        if let Some((punct, next_cursor)) = group_cursor.punct() {
+            if punct.as_char() == '~' {
+                if let Some((ident, next_cursor)) = next_cursor.ident() {
+                    if &ident == self.base.loop_ident {
+                        if let Some(TokenTree::Ident(prefix)) = self.base.collected_tokens.last() {
+                            let concat_str = prefix.to_string() + &current_index.to_string();
+                            // Need to pop the last ident, will be replaced by completely new ident
+                            self.base.collected_tokens.pop();
+                            self.base
+                                .collected_tokens
+                                .push(Ident::new(&concat_str, Span::call_site()).into());
+                            *group_cursor = next_cursor;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        if let Some((mut inner_group, gspan, next_cursor)) = group_cursor.group(Parenthesis) {
+            self.handle_group_cursor_inner(Parenthesis, &mut inner_group, gspan, current_index);
+            *group_cursor = next_cursor;
+            return;
+        }
+        if let Some((mut inner_group, gspan, next_cursor)) = group_cursor.group(Brace) {
+            self.handle_group_cursor_inner(Brace, &mut inner_group, gspan, current_index);
+            *group_cursor = next_cursor;
+            return;
+        }
+        let (tt, next_cursor) = group_cursor
+            .token_tree()
+            .expect("Inner cursor parsing configuration error. Reached unexpected EOF");
+        self.base.collected_tokens.push(tt);
+        *group_cursor = next_cursor;
+    }
+
+    fn handle_group_cursor_outer(
+        &mut self,
+        delim: Delimiter,
+        group_cursor: &mut Cursor,
+        gspan: Span,
+    ) {
+        let mut group_tt_collector =
+            TtCollectorInnerReps::new(self.base.loop_ident, self.start, self.end);
+        while !group_cursor.eof() {
+            group_tt_collector.handle_cursor(group_cursor);
+        }
+        let mut group_token = Group::new(delim, group_tt_collector.consume());
+        group_token.set_span(gspan);
+        self.base.collected_tokens.push(group_token.into());
+    }
+
+    fn handle_group_cursor_inner(
+        &mut self,
+        delim: Delimiter,
+        group_cursor: &mut Cursor,
+        gspan: Span,
+        current_index: usize,
+    ) {
+        let mut group_tt_collector =
+            TtCollectorInnerReps::new(self.base.loop_ident, self.start, self.end);
+        while !group_cursor.eof() {
+            group_tt_collector.handle_inner_rep_cursor(group_cursor, current_index);
+        }
+        let mut group_token = Group::new(delim, group_tt_collector.consume());
+        group_token.set_span(gspan);
+        self.base.collected_tokens.push(group_token.into());
     }
 }
