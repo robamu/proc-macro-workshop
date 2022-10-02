452ef1687b325d0b55cec32bf0f7173e00dc6c94
Robin Mueller <robin.mueller.m@gmail.com>
2022-10-02 16:10:59 +0200

    complete 02

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,20 +1,28 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
 use quote::ToTokens;
-use syn::parse_macro_input;
+use syn::{parse_macro_input, AttributeArgs, Item};
 
 #[proc_macro_attribute]
 pub fn sorted(
     args: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
-    let _ = args;
-    let input_copy = input.clone();
-    let input_item = parse_macro_input!(input_copy as syn::Item);
-    process_input(input_item)
+    let args = parse_macro_input!(args as AttributeArgs);
+    let input_item = parse_macro_input!(input as Item);
+    process_input(args, input_item)
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
 }
 
-fn process_input(input: syn::Item) -> syn::Result<TokenStream> {
+fn process_input(args: AttributeArgs, input: Item) -> syn::Result<TokenStream> {
+    match input {
+        Item::Enum(_) => {}
+        _ => {
+            return Err(syn::Error::new(
+                Span::call_site(),
+                "expected enum or match expression",
+            ))
+        }
+    }
     Ok(input.to_token_stream())
 }
